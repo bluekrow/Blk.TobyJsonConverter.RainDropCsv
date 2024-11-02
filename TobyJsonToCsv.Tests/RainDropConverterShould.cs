0cabc074c03288a8c839efcb988e3a678127290e
BlueKrow <ndioses@gmail.com>
2024-11-02 18:34:25 +0100

    test: Raindrop converter produces csv lines correctly

---
+++ TobyJsonToCsv.Tests/RainDropConverterShould.cs
@@ -0,0 +1,22 @@
+using System.Text.Json;
+using TobyJsonToCsv.RainDrop;
+
+namespace TobyJsonToCsv.Tests;
+
+public class RainDropConverterShould
+{
+    private static readonly JsonSerializerOptions jsonSerializerOptions = new()
+    {
+        IncludeFields = true,
+        PropertyNameCaseInsensitive = true
+    };
+    
+    [Fact]
+    public Task ProduceRainDropCsvLines()
+    {
+        var sourceTobyExportedText = File.ReadAllText("Resources/sourceTobyExported.json");
+        var tobyObject = JsonSerializer.Deserialize<TobyObject>(sourceTobyExportedText, jsonSerializerOptions);
+        var convertedRainDropActualLines = tobyObject!.ToRainDropCsv().GetCsvLines();
+        return Verify(convertedRainDropActualLines);
+    }   
+}
