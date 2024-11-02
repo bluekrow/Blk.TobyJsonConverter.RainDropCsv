3c474b4415723093ad4f8f082433453a638cdf99
BlueKrow <ndioses@gmail.com>
2024-11-02 19:14:08 +0100

    fix: test app files generated locally so it works with relative paths

--- TobyJsonToCsv.Tests/ApplicationShould.cs
+++ TobyJsonToCsv.Tests/ApplicationShould.cs
@@ -5,8 +5,17 @@ public class ApplicationShould
     [Fact]
     public Task TemporarilyMaintainFailedExport()
     {
-        Program.Main([]);
-        return VerifyFile("/home/raven/Downloads/toby-export-sample.csv");
+        const string SOURCE_TOBY_EXPORTED_JSON = "Resources/sourceTobyExported.json";
+        const string RAINDROP_CONVERTED_CSV = "Resources/rainDropConverted.csv";
+
+        var programArguments = new []
+        {
+            SOURCE_TOBY_EXPORTED_JSON,
+            RAINDROP_CONVERTED_CSV
+        };
+        
+        Program.Main(programArguments);
+        return VerifyFile(RAINDROP_CONVERTED_CSV);
     }
 
     [Fact (Skip = "In progress goal")]
