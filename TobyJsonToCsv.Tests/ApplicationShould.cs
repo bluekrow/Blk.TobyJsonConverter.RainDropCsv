d283989d69dc211e5cedb4381278ddd542ae02f3
BlueKrow <ndioses@gmail.com>
2024-11-02 15:37:33 +0100

    refactor: add application arguments support

--- TobyJsonToCsv.Tests/ApplicationShould.cs
+++ TobyJsonToCsv.Tests/ApplicationShould.cs
@@ -5,14 +5,14 @@ public class ApplicationShould
     [Fact]
     public Task TemporarilyMaintainFailedExport()
     {
-        Program.Main(default!);
+        Program.Main([]);
         return VerifyFile("/home/raven/Downloads/toby-export-sample.csv");
     }
 
     [Fact (Skip = "In progress goal")]
     public Task ConvertATobyJsonToRainDropCsv()
     {
-        Program.Main(default!);
+        Program.Main([]);
         return VerifyFile("/home/raven/Downloads/toby-export-sample.csv");
     }
 }
--- TobyJsonToCsv/Program.cs
+++ TobyJsonToCsv/Program.cs
@@ -8,18 +8,24 @@ namespace TobyJsonToCsv
     // ReSharper disable once ClassNeverInstantiated.Global
     internal class Program
     {
+        private static readonly JsonSerializerOptions jsonSerializerOptions = new()
+        {
+            IncludeFields = true,
+            PropertyNameCaseInsensitive = true
+        };
+
         public static void Main(string[] args)
         {
-            var tobyJsonContent = File.ReadAllText("/home/raven/Downloads/toby-export-sample.json");
-            var jsonSerializerOptions = new JsonSerializerOptions
-            {
-                IncludeFields = true,
-                PropertyNameCaseInsensitive = true
-            };
+            const string SOURCE_TOBY_EXPORTED_JSON = "/home/raven/Downloads/toby-export-sample.json";
+            const string CONVERTED_RAINDROP_CSV = "/home/raven/Downloads/toby-export-sample.csv";
+            var sourceTobyExportedJson= args.Length > 0 && !string.IsNullOrWhiteSpace(args[0]) ? args[0] : SOURCE_TOBY_EXPORTED_JSON;
+            var convertedRaindropCsv = args.Length > 1 && !string.IsNullOrWhiteSpace(args[1]) ? args[1] : CONVERTED_RAINDROP_CSV;
+
+            var tobyJsonContent = File.ReadAllText(sourceTobyExportedJson);
             var tobyObject = JsonSerializer.Deserialize<TobyObject>(tobyJsonContent, jsonSerializerOptions);
             var rainDropCsv = tobyObject!.ToRainDropCsv();
-
-            File.WriteAllLines("/home/raven/Downloads/toby-export-sample.csv", rainDropCsv.GetCsvLines());
+          
+            File.WriteAllLines(convertedRaindropCsv, rainDropCsv.GetCsvLines());
         }
     }
 }
