a7ff496dbab205ff353af4a485d5ed92d19b65ab
BlueKrow <ndioses@gmail.com>
2024-11-03 15:28:41 +0100

    test: remove temporary test

--- TobyJsonToCsv.Tests/ApplicationShould.cs
+++ TobyJsonToCsv.Tests/ApplicationShould.cs
@@ -3,25 +3,18 @@ namespace TobyJsonToCsv.Tests;
 public class ApplicationShould
 {
     [Fact]
-    public Task TemporarilyMaintainFailedExport()
+    public Task ConvertATobyJsonToRainDropCsv()
     {
         const string SOURCE_TOBY_EXPORTED_JSON = "Resources/sourceTobyExported.json";
         const string RAINDROP_CONVERTED_CSV = "Resources/rainDropConverted.csv";
 
-        var programArguments = new []
+        var programArguments = new[]
         {
             SOURCE_TOBY_EXPORTED_JSON,
             RAINDROP_CONVERTED_CSV
         };
-        
+
         Program.Main(programArguments);
         return VerifyFile(RAINDROP_CONVERTED_CSV);
-    }
-
-    [Fact (Skip = "In progress goal")]
-    public Task ConvertATobyJsonToRainDropCsv()
-    {
-        Program.Main([]);
-        return VerifyFile("/home/raven/Downloads/toby-export-sample.csv");
     }
 }
